504323a414acd6890dfefcd364631584b3d1a08c
Eduavite123 <eduavilestejada@gmail.com>
2025-04-03 21:26:00 +0200

    comp made

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ Resumen del trabajo: (EN PROCESO)
 
 ## Scripts
 
+**read_cdfs.ipynb**: Notebook auxiliar para leer información de archivos NETCDFs.
+
 ### Generación de los datos de rocío recolectable
 **netcdfs_dewyield_CERRA.py**: Script que genera una archivo NETCDF para cada año con los datos de rocío recolectable por hora obtenidos de los datos de CERRA cada seis horas entre 1991 y 2020.
+
 **netcdfs_dewyield_WRF**: Script igual al anterior con los datos de WRF.
+
 (faltan los atributos en los NETCDFs, si son necesarios se añadirán en el futuro)
 
 ### Estudio de los campos medios
 **CERRA_WRF_comp.py**: (EN PROCESO) Script que genera un plot que compara la media de rocío recolectable al año en el periodo 1991-2020 de los datos CERRA y el modelo WRF. Incluye la interpolación de los datos WRF para adaptarlos a la rejilla de CERRA (de 5km a 5,5km) y la generación de archivos NETCDFs con las medias correspondientes para su futuro análisis.
+
+## Figuras
+**CERRA_WRF_dewyiled.png** Comparación lado a lado de la media de producción de rocío en el periodo 1991-2020 en el modelo de WRF y el reanálisis de CERRA.
--- scripts/CERRA_WRF_comp.py
+++ scripts/CERRA_WRF_comp.py
@@ -4,27 +4,98 @@ import xarray as xr
 import numpy as np
 from matplotlib import pyplot as plt
 import cartopy.crs as ccrs
+import os
+from scipy.interpolate import griddata
 
 #--------------------------------------------PLOT--------------------------------------------------------------
-def plot_dual(lons_1, lats_1, lons_2, lats_2, h_mean1, h_mean2, title1, title2, general_title, outroute):
+def plot_dual(lons, lats, h_mean1, h_mean2, label, title1, title2, general_title, outroute):
     fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': ccrs.LambertConformal(central_longitude=3)}, figsize=(18, 8))
     fig.suptitle(general_title, fontsize=16, x=0.515)
 
     ax1.set_extent([-10, 4.5, 35.97, 44.25], crs=ccrs.PlateCarree()) # zoom a península ibérica
     ax1.coastlines()
-    c1 = ax1.pcolormesh(lons_1, lats_1, h_mean1, cmap='viridis', shading='auto', transform=ccrs.PlateCarree())
+    c1 = ax1.pcolormesh(lons, lats, h_mean1, cmap='viridis', shading='auto', transform=ccrs.PlateCarree())
     ax1.set_title(title1)
 
     ax2.set_extent([-10, 4.5, 35.97, 44.25], crs=ccrs.PlateCarree()) # zoom a península ibérica
     ax2.coastlines()
-    c2 = ax2.pcolormesh(lons_2, lats_2, h_mean2, cmap='viridis', shading='auto', transform=ccrs.PlateCarree())
+    c2 = ax2.pcolormesh(lons, lats, h_mean2, cmap='viridis', shading='auto', transform=ccrs.PlateCarree())
     ax2.set_title(title2)
 
     cbar = fig.colorbar(c1, ax=[ax1, ax2], orientation='horizontal', shrink=0.8, pad=0.05)
-    cbar.set_label('Producción de rocío por hora (mm/h)')
+    cbar.set_label(label)
     #fig.subplots_adjust(wspace=0.05, bottom=0.25, top=0.85)  
 
     plt.savefig(outroute, dpi=300, bbox_inches='tight')
     plt.close(fig)
-#-------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------
 
+#----------------------------------------MEAN DEW YIELD--------------------------------------------------------
+def mean_dewyield(input_folder):
+    #Creamos lista con los archivos
+    files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('.nc')]
+    #Cargamos y concatenamos los archivos
+    mean_sum=0
+    count=0
+    for f in files:
+        with xr.open_dataset(f) as ds:
+            mean_sum += ds['dew_yield'].mean(dim='time')
+            count += 1
+    #Calculamos la media 
+    mean_dewyield = mean_sum / count
+    #Pasamos de mm/h a mm/año
+    return mean_dewyield*24*365
+#--------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------MAIN--------------------------------------------------
+#Media del periodo CERRA
+input_folder='scripts\\dewyield_results\\CERRA'
+mean_dewyield_C=mean_dewyield(input_folder)
+#Media del periodo WRF
+input_folder='scripts\\dewyield_results\\WRF'
+mean_dewyield_W=mean_dewyield(input_folder)
+
+#extraemos latitudes y longitudes que serán necesarias
+#WRF
+fn='scripts\\dewyield_results\\WRF\\dew_yield_WRF_1991.nc'
+wrff=xr.open_dataset(fn)
+lons_WRF=wrff.coords['longitude'].values
+lats_WRF=wrff.coords['latitude'].values
+wrff.close()
+#CERRA
+fn='scripts\\dewyield_results\\CERRA\\dew_yield_CERRA_1991.nc'
+cerraf=xr.open_dataset(fn)
+lons_C=cerraf.coords['longitude'].values
+lats_C=cerraf.coords['latitude'].values
+cerraf.close()
+#Usaremos también la máscara de mar en la interpolación
+fn='scripts\\data\\CERRA\\CERRA_IP_3h_1991.nc'
+cerraf=xr.open_dataset(fn)
+land_mask=cerraf.data_vars['lsm'].values[0,:,:]
+cerraf.close()
+land_mask=np.where(land_mask==0.0, np.nan, land_mask)
+
+#.................Interpolación de datos WRF....................
+#Creación de la matriz con dimensiones de CERRA
+[nlat, nlon]=mean_dewyield_C.shape
+h_WRF_regrid=np.full((nlat, nlon), np.nan)
+
+#Máscara de valores válidos
+mask=~np.isnan(mean_dewyield_W.values)
+
+#Extraemos los datos válidos con la máscara
+lat = lats_WRF[mask]
+lon = lons_WRF[mask]
+h_mean = mean_dewyield_W.values[mask]
+
+#Interpolación de los datos WRF a la malla de CERRA
+h_WRF_regrid = griddata((lon,lat), h_mean, (lons_C, lats_C), method='linear')
+
+mean_dewyield_W = h_WRF_regrid*land_mask
+#...............................................................
+
+#Creamos el plot
+leyenda='Producción de rocío (mm/año)'
+title= 'Producción de rocío recolectable entre 1991 y 2020'
+outroute='figures\\CERRA_WRF_dewyield.png'
+plot_dual(lons_C, lats_C, mean_dewyield_C, mean_dewyield_W, leyenda, 'CERRA', 'WRF', title, outroute)
