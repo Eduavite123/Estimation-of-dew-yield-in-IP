b0f78b83c192162bb1a87f6e13ae66c9544553f8
Eduavite123 <eduavilestejada@gmail.com>
2025-05-21 17:00:44 +0200

    update

--- README.md
+++ README.md
@@ -13,14 +13,13 @@ Resumen del trabajo: (EN PROCESO)
 (faltan los atributos en los NETCDFs, si son necesarios se añadirán en el futuro)
 
 ### Estudio de los campos medios
-**CERRA_WRF_comp.py**: Script con varias funciones:
+**comp_test.py**: Script con varias funciones:
 - Genera un plot que compara la media de rocío recolectable al año en el periodo 1991-2020 de los datos CERRA y el modelo WRF.
 - Calcula y genera un plot de la diferencia relativa de las medias de los modelos. Realiza también un test U de Mann-Whitney.
-- Calcula las desviaciones típicas y genera un plot del cociente de las desviaciones de ambos modelos. Realiza también un test F de Snedecor.
 
 ## Figuras
-**CERRA_WRF_dewyiled.png** Comparación lado a lado de la media de producción de rocío en el periodo 1991-2020 en el modelo de WRF y el reanálisis de CERRA.
+**CERRA_WRF_dy.png** Comparación lado a lado de la media de producción de rocío en el periodo 1991-2020 en el modelo de WRF y el reanálisis de CERRA.
 
-**dif_relativas.png** Diferencias relativas de la media de producción de rocío en la PI entre el modelo WRF y los datos CERRA.
+**CERRA_WRF_dy_diff.png** Diferencias relativas de la media de producción de rocío en la PI entre el modelo WRF y los datos CERRA.
 
-**coc_var.png** Mapa de cociente de varianzas
+**p_value_dy.png** Mapa de valores de p-value resultado del test U de Mann Whitney
--- scripts/comp_test.py
+++ scripts/comp_test.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cartopy.crs as ccrs
 from scipy.stats import mannwhitneyu
-from scipy.stats import f 
-from datetime import datetime
+from scipy.io import loadmat
+import h5py
 
 #--------------------------------------------DUAL PLOT--------------------------------------------------------------
 #Función que genera el gráfico (mapas de la PI) de comparación lado a lado
@@ -56,25 +56,28 @@ def plot(lons, lats, variable, label, color, outroute):
 
 #----------------------------------------------MAIN-----------------------------------------------------------
 #extraemos latitudes y longitudes para los plots
-fn='scripts\\dewyield_results\\CERRA\\dew_yield_CERRA_1991.nc'
+fn='scripts\\data\\CERRA\\CERRA_IP_3h_1991.nc'
 cerraf=xr.open_dataset(fn)
 lons=cerraf.coords['longitude'].values
 lats=cerraf.coords['latitude'].values
 cerraf.close()
+lats=lats.transpose() #los archivos mat tienen las coordenadas al revés
+lons=lons.transpose()
 #por si no está aplicada la máscara de tierra, la cargamos también
 fn='scripts\\data\\land_mask.nc'
 cerraf=xr.open_dataset(fn)
 land_mask=cerraf['lsm'].values
 cerraf.close()
 land_mask=np.where(land_mask==0, np.nan, land_mask) #convertimos a NaN los valores de mar
+land_mask=land_mask[0,:,:].transpose()
 
 #Rutas y datos de los archivos a usar en el script
-fn1='scripts\\dewyield_results\\dewyield_CERRA_daysum.nc'
-fn2='scripts\\dewyield_results\\dewyield_WRF_daysum.nc'
-selname1='dew_yield'
-selname2='dew_yield'
+# fn1='scripts\\dewyield_results\\dewyield_CERRA_daysum.nc'
+# fn2='scripts\\dewyield_results\\dewyield_WRF_daysum.nc'
+# selname1='dew_yield'
+# selname2='dew_yield'
 
-land_mask_apply=False #True para aplicar, False para no
+land_mask_apply=True #True para aplicar, False para no
 
 #nombres de los archivos de figuras y sus títulos y leyendas
 label_comp='Producción de rocío (mm/año)' #leyenda comparación lado a lado
@@ -83,24 +86,66 @@ out_name_comp='figures\\CERRA_WRF_dy.png' #nombre del archivo de comparación
 title1='CERRA' #título del primer gráfico
 title2='WRF' #título del segundo gráfico
 
+out_name_dif='CERRA_WRF_dy_diff.png' #nombre del archivo de diferencias relativas
+
 #...........................PLOT DE COMPARACIÓN..........................
 #Cargamos los sets con datos diarios
-f1=xr.open_dataset(fn1)
-mod1=f1[selname1].values
-f1.close()
-f2=xr.open_dataset(fn2)
-mod2=f2[selname2].values
-f2.close()
+# f1=xr.open_dataset(fn1)
+# mod1=f1[selname1].values
+# f1.close()
+# f2=xr.open_dataset(fn2)
+# mod2=f2[selname2].values
+# f2.close()
+
+f = h5py.File('scripts\\dewyield_results\\dewyield_CERRA_data.mat','r')
+data = f.get('daily_dewyield')
+mod1 = np.array(data)
+f = h5py.File('scripts\\dewyield_results\\dewyield_WRF_Evaluation_data_regridded.mat','r')
+data = f.get('daily_dewyield')
+mod2 = np.array(data)
+print('Datos cargados')
 
 #Realizamos las medias 
 if land_mask_apply==True:
-    mean1=np.nanmean(mod1, axis=0) * land_mask
-    mean2=np.nanmean(mod2, axis=0) * land_mask
+    mean1=np.nanmean(mod1, axis=2) * land_mask
+    mean2=np.nanmean(mod2, axis=2) * land_mask
 else:
-    mean1=np.nanmean(mod1, axis=0)
-    mean2=np.nanmean(mod2, axis=0)
+    mean1=np.nanmean(mod1, axis=2)
+    mean2=np.nanmean(mod2, axis=2)
+
+mean2=np.where(mean2>0.05, np.nan, mean2)
+print('Medias calculadas')
 
 #Hacemos el plot
 plot_dual(lons, lats, mean1, mean2, label_comp, title1, title2, out_name_comp)
+print('Plot de comparación realizado')
 #...........................................................................
 
+#...........................TEST U DE MANN WHITNEY..........................
+_, p_value = mannwhitneyu(mod1, mod2, alternative='two-sided', axis=2, nan_policy='omit')
+print(p_value.shape)
+print('Test de Mann Whitney realizado')
+
+plot(lons, lats, p_value, 'p-value', 'viridis', 'figures\\p_value_dy.png')
+print('Plot de p-value realizado')
+
+#Realizamos una máscara para celdas donde las diferencias son significativas
+p_value_mask=np.where(p_value<0.05, 1, np.nan) #nivel de significancia 95%
+#...........................................................................
+
+#...........................DIFERENCIAS RELATIVAS...........................
+#Cálculo de diferencias relativas
+dif_rel=(mean2-mean1)/mean1*100
+print('Diferencias relativas calculadas')
+
+#Aplicamos la máscara de p-value
+dif_rel=dif_rel*p_value_mask
+
+dif_rel=np.where(dif_rel>600, np.nan, dif_rel)
+
+#Plot de diferencias relativas
+label_difrel='Diferencia relativa (%)'
+outroute=f'figures\\{out_name_dif}'
+plot(lons, lats, dif_rel, label_difrel, 'rainbow', outroute)
+print('Plot de diferencias relativas realizado')
+#...........................................................................
